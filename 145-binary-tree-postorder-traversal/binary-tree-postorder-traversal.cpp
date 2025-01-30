e863e32c7ae3631fb2a0ffd0b35e91e93e212dbe
Tejas Bali <84762080+tejas1826@users.noreply.github.com>
2025-01-30 14:42:22 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (44.39%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        dfs(root, result);
+        return result;
+    }
+    
+    void dfs(TreeNode* node, vector<int>& result) {
+        if (!node) return;
+        dfs(node->left, result);      // Traverse left subtree
+        dfs(node->right, result);     // Traverse right subtree
+        result.push_back(node->val);  // Visit root
+    }
+};
